a823aa4eb1cc4df490dd21d74ed2a068b8732959
Victor Tang <vtange@users.noreply.github.com>
2021-08-09 23:29:25 -0700

    wired up the timer

--- countdown/index.js
+++ countdown/index.js
@@ -1,6 +1,31 @@
-var debutG = 1628434800000;
-var debutL = 1628694000000;
 var debutN = 1628780400000;
-var debutB = 1628866800000;
 var debutP = 1629039600000;
-var time = new Date();
+var debutL = 1628694000000;
+var debutB = 1628866800000;
+var debuts = [debutN,debutP,debutL,debutB];
+function secondsToHms(d) {
+    d = Number(d);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+    return (
+      (h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s
+    );
+}
+function get(a){
+    return document.getElementById(a);
+}
+function runTimer(){
+    var time = new Date().getTime();
+    nplb.forEach(function(el,i){
+        el.innerHTML = secondsToHms((debuts[i] - time)/1000);
+    });
+}
+var ne = get("ne");
+var po = get("po");
+var ra = get("ra");
+var bo = get("bo");
+var nplb = [ne,po,ra,bo];
+window.setInterval(function(){
+    runTimer();
+},1000);
