fc8c20a289b91bd679486272ec6dad21153927a7
Victor Tang <vtange@users.noreply.github.com>
2022-03-05 08:47:06 -0800

    switch click to bday site for nene

--- nene/_nuxt/static/1646302045/en/hub/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnene\u002F_nuxt\u002Fstatic\u002F1646302045",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Fhub",config:{_app:{basePath:"\u002Fnene\u002F",assetsPath:"\u002Fnene\u002F_nuxt\u002F",cdnURL:a}}}}(null));
