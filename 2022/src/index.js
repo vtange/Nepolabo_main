e4d85177a752a033daabb514a3b45e68dea5f12a
Victor Tang <vtange@users.noreply.github.com>
2022-08-11 11:01:37 -0700

    update deadlines, some styling

--- 2022/src/index.js
+++ 2022/src/index.js
@@ -147,10 +147,10 @@ sky.addEventListener("click", function(e){
 });
 
 // countdown stuff
-var debutN = 1628780400000+31557600000;
-var debutP = 1629039600000+31557600000;
-var debutL = 1628694000000+31557600000;
-var debutB = 1628866800000+31557600000;
+var debutN = 1628780400000+31557600000-10800000;
+var debutP = 1629039600000+31557600000-10800000;
+var debutL = 1628694000000+31557600000-10800000;
+var debutB = 1628866800000+31557600000-10800000;
 var debuts = [debutN,debutP,debutL,debutB];
 function secondsToHms(d) {
     d = Number(d);
